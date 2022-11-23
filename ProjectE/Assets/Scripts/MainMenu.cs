a4fb43eabec3052ed27113c06125cb3086d93d45
gslant <77141272+gslant@users.noreply.github.com>
2022-11-23 19:06:21 +1300

    main menu buttons and functionality set up

--- ProjectE/Assets/Scripts/MainMenu.cs
+++ ProjectE/Assets/Scripts/MainMenu.cs
@@ -7,16 +7,41 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public Button startButton;
+    public Button startButton, settingsButton, quitButton;
+    public Button backToMainButton;
+    public GameObject mainPanel, settingsPanel;
 
     void Start()
     {
+        settingsPanel.SetActive(false);
+        mainPanel.SetActive(true);
         startButton.onClick.AddListener(delegate { StartGame(); });
+        settingsButton.onClick.AddListener(delegate { GoToSettings(); });
+        quitButton.onClick.AddListener(delegate { QuitGame(); });
+        backToMainButton.onClick.AddListener(delegate { BackToMainMenu(); });
     }
 
     private void StartGame()
     {
         SceneManager.LoadScene("Game");
+    }
+
+    private void GoToSettings()
+    {
+        settingsPanel.SetActive(true);
+        mainPanel.SetActive(false);
+    }
+
+    private void QuitGame()
+    {
+        Debug.Log("Application.quit, will work in real build");
+        Application.Quit();
+    }
+
+    private void BackToMainMenu()
+    {
+        settingsPanel.SetActive(false);
+        mainPanel.SetActive(true);
     }
 
 
