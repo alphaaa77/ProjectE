60a7b18067b228f1954892bdc78bf6a12aeccaa2
Hugh Magallanes <yzn8334@autuni.ac.nz>
2022-11-23 18:27:05 +1300

    added jump buttons

---
+++ ProjectE/Assets/Scripts/Player Scripts/PlayerScript.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float JumpForce;
+    [SerializeField] bool isGrounded = false;
+    Rigidbody2D rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(isGrounded == true)
+            {
+                rigidBody.AddForce(Vector2.up * JumpForce);
+                isGrounded = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("Ground"))
+        {
+            if(isGrounded == false)
+            {
+                isGrounded = true;
+            }
+        }
+    }
+}
